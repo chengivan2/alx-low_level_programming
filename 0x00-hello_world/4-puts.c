03610d0e17136ceeea5c0315082f1478fda0390d
chengivan2 <chengoleivan@gmail.com>
2023-07-13 02:18:36 -0700

    Followed Betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
-int main()
+/**
+ * main is the entry point
+ * This program outputs a statement
+ * 
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
