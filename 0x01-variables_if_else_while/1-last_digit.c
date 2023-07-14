b3b8a412b3d44f37937008dff20c59e3f9fbc629
chengivan2 <chengoleivan@gmail.com>
2023-07-14 05:29:54 -0700

    IF else task

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: Tell if last digit is >, == or <
+ *
+ * Return: always 0 success
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	int last_d = n % 10;
+
+	if (last_d > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_d);
+	}
+	else if (last_d == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, last_d);
+	}
+	else if (last_d < 6 && last_d != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_d);
+	}
+
+	return (0);
+}
