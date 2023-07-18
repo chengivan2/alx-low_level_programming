48f4c392c6fb654ae35c40360edc15062eb52af1
chengivan2 <chengoleivan@gmail.com>
2023-07-18 11:08:34 -0700

    Hot Fix 2 for INT_MIN

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,20 +6,18 @@
  *
  * Return: The value of the last digit
  */
+
 int print_last_digit(int n)
 {
 	int last_digit;
 
-	if (n < 0)
-	{
-		n = -n;
-	}
-
-	last_digit = n % 10;
-
-	if (last_digit < 0)
-	{
-		last_digit = last_digit * -1
+	if (n == INT_MIN) {
+		last_digit = 8; /* Last digit of INT_MIN is 8 */
+	} else {
+		if (n < 0) {
+			n = -n;
+		}
+		last_digit = n % 10;
 	}
 
 	_putchar(last_digit + '0');
