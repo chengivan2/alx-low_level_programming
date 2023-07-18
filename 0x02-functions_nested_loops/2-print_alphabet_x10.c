eb0f4fe4f3bb5fdd43b1122ad927b8bdc85c2dc6
chengivan2 <chengoleivan@gmail.com>
2023-07-18 10:31:18 -0700

    Fixed betty over 80 lines

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Prints the lowercase alphabet 10 times followed by a new line
+ * The func - Prints the lowercase alphabet 10 times followed by a new line
  */
 void print_alphabet_x10(void)
 {
