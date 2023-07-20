ffb2538778c06e89b69db42d540ca512bdaa9fa8
chengivan2 <chengoleivan@gmail.com>
2023-07-20 08:05:10 -0700

    Test positiveornegative function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Tests function that prints if number is positive or negative
+ *
+ * Return: 0 means success and 1 means error
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0)
+}
