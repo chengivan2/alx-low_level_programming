f06725e5ce93943140c105209277bc1e19661dbd
chengivan2 <chengoleivan@gmail.com>
2023-07-24 09:45:41 -0700

    Hot fix for long input

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,11 +10,11 @@ void puts2(char *str)
 {
 	int i;
 
-	if (str)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (i = 0; str[i]; i += 2)
+		if (i % 2 == 0)
 			_putchar(str[i]);
+	}
 
 		_putchar('\n');
-	}
 }
