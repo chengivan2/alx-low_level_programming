53df77b570671f3b14b818a6f01e781daee926e4
chengivan2 <chengoleivan@gmail.com>
2023-07-24 10:29:02 -0700

    a million minutes later

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * print_array - prints n elements of an array of integers, followed by a new line.
+ * print_array - prints n elements of an array of integers, followed by \n
  * @a: pointer to the array.
  * @n: number of elements to print.
  *
@@ -11,18 +11,12 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	if (n <= 0 || a == NULL)
-	{
-		/* Invalid input or empty array, nothing to print */
-		return;
-	}
-
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
 
-		/* Print a comma and space for all elements except the last one */
-		if (i < n - 1)
+		
+		if (i != (n - 1))
 		{
 			printf(", ");
 		}
