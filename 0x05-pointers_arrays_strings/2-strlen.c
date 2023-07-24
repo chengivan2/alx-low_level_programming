fb1235b833236ae281938051e62d16fca2f3a65e
chengivan2 <chengoleivan@gmail.com>
2023-07-23 21:06:52 -0700

    Count length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns length of string
+ *
+ * @s - String to determine length
+ *
+ * Return: returns value of cnt
+ */
+
+int _strlen(char *s)
+{
+	int cnt = 0;
+
+	for(; *s != '\0'; s++)
+	{
+		cnt++;
+	}
+
+	return (cnt);
+}
