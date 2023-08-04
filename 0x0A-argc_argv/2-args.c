fefee4561a58fe3b51a160b046d604d8f5201b35
chengivan2 <chengoleivan@gmail.com>
2023-08-04 09:57:30 -0700

    hot fix1

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,8 +3,8 @@
  * main - Write a program that prints all arguments it receives
  *        All arguments should be printed, including the first on
  *
- * @argc: This is the argument count 
- * @argv: This is the argument vector
+ *@argc: This is the argument count
+ *@argv: This is the argument vector
  *
  * Return: 0;
  */
